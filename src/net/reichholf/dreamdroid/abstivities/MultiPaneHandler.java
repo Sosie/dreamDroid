9afa4c3edaf6ed62a94ad9b560d91fa924a3f2dd
sre <stephan@reichholf.net>
2013-05-23 21:11:55 +0200

    * add/update copyright in java files

--- src/net/reichholf/dreamdroid/abstivities/MultiPaneHandler.java
+++ src/net/reichholf/dreamdroid/abstivities/MultiPaneHandler.java
@@ -1,7 +1,13 @@
-/* © 2010 Stephan Reichholf <stephan at reichholf dot net>
- * 
+/*
+ * Copyright © 2013. Stephan Reichholf
+ *
+ * Unless stated otherwise in a files head all java and xml-code of this Project is:
+ *
  * Licensed under the Create-Commons Attribution-Noncommercial-Share Alike 3.0 Unported
  * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ *
+ * All grahpics, except the dreamdroid icon, can be used for any other non-commercial purposes.
+ * The dreamdroid icon may not be used in any other projects than dreamdroid itself.
  */
 
 package net.reichholf.dreamdroid.abstivities;
--- src/net/reichholf/dreamdroid/helpers/EasyX509TrustManager.java
+++ src/net/reichholf/dreamdroid/helpers/EasyX509TrustManager.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright © 2013. Stephan Reichholf
+ *
+ * Unless stated otherwise in a files head all java and xml-code of this Project is:
+ *
+ * Licensed under the Create-Commons Attribution-Noncommercial-Share Alike 3.0 Unported
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ *
+ * All grahpics, except the dreamdroid icon, can be used for any other non-commercial purposes.
+ * The dreamdroid icon may not be used in any other projects than dreamdroid itself.
+ */
+
 package net.reichholf.dreamdroid.helpers;
 
 /*
